ad334c5587615a61f608ba9dec7b8001e1dee4d1
Jennifer Weir <contact@jenniferpweir.com>
2026-06-07 21:31:17 -0400

    fix: EXIT newline removal

--- client.py
+++ client.py
@@ -9,11 +9,11 @@ def main():
 
         # run in a loop to continuously prompt the user for input to send to the server until they choose to exit
         while True:
-            message = input("Enter command (`PUT key value` or `GET key`): ") + "\n"
+            message = input("Enter command (`PUT key value` or `GET key`): ").strip()
             # close the connection if the user types "EXIT" signifying they want to disconnect
             if message == "EXIT":
                 break
-            client_socket.sendall(message.encode('utf-8'))
+            client_socket.sendall((message + "\n").encode('utf-8'))
 
             response = client_socket.recv(1024).decode('utf-8').strip()
             print(f"Server response: {response}")
